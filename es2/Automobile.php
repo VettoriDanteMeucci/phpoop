3ffe9ce5a3585d621fd00cd31379f0d31d370c67
lai2-24 <lai2-24@itismeucci.com>
2025-02-11 12:39:04 +0100

    es2 completed

---
+++ es2/Automobile.php
@@ -0,0 +1,16 @@
+<?php 
+    include("./Veicolo.php");
+    class Automobile extends Veicolo{
+        private $modello;
+
+        public function __construct($anno, $marca, $modello) {
+            parent::__construct($anno, $marca);
+            $this->modello = $modello;
+        }
+
+        public function getModello(){
+            return $this->modello;
+        }
+
+    }
+?>
